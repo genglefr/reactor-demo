1ca44d06e922eb1a089c7b9728d7d2670be6c8fb
genglefr <Anderlecht8>
2018-08-24 12:16:07 +0200

    Turn the page into a live score dashboard

--- src/main/resources/cb-design-doc.js
+++ src/main/resources/cb-design-doc.js
@@ -1,17 +1,17 @@
 //_design/pet/all
-function (doc, meta) {
+function(doc, meta) {
     if(doc._class === "com.genglefr.webflux.demo.model.Pet"){
         emit(meta.id, null);
     }
 }
 //_design/user/all
-function (doc, meta) {
+function(doc, meta) {
     if(doc._class === "com.genglefr.webflux.demo.model.User"){
         emit(meta.id, null);
     }
 }
 //_design/game/all
-function (doc, meta) {
+function(doc, meta) {
     if(doc._class === "com.genglefr.webflux.demo.model.Game"){
         emit(meta.id, null);
     }
