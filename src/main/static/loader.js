07d781960de29d6164710db5c2ff2f1bbc6b5730
genglefr <nufann@gmail.com>
2018-10-02 12:12:13 +0200

    Smart script loading function

--- src/main/static/loader.js
+++ src/main/static/loader.js
@@ -4,7 +4,7 @@ function loadScript(url) {
     document.head.appendChild(script);
     return new Promise(function(resolve, reject) {
         script.onload = function() {
-            console.log("Script loaded: " + url);
+            console.log("Script loaded: '" + url + "'");
             resolve();
         };
     });
