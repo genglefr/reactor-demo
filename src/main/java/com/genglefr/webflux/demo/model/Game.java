4a22de2ca1f823eda036ac5000cd343c270dcc64
genglefr <nufann@gmail.com>
2018-10-15 18:37:36 +0200

    code review

--- src/main/java/com/genglefr/webflux/demo/model/Game.java
+++ src/main/java/com/genglefr/webflux/demo/model/Game.java
@@ -7,7 +7,6 @@ import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.core.mapping.id.GeneratedValue;
 import org.springframework.data.couchbase.core.mapping.id.GenerationStrategy;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Random;
@@ -15,6 +14,7 @@ import java.util.Random;
 @Document
 @Data
 public class Game implements Entity {
+    private static final int max_score = 5;
     @Id
     @GeneratedValue(strategy = GenerationStrategy.UNIQUE)
     String id;
@@ -44,9 +44,8 @@ public class Game implements Entity {
 
     public Game randomize() {
         Random rand = new Random();
-        int bound = 5;
-        this.teamHomeScore = rand.nextInt(bound);
-        this.teamAwayScore = rand.nextInt(bound);
+        this.teamHomeScore = rand.nextInt(max_score);
+        this.teamAwayScore = rand.nextInt(max_score);
         return this;
     }
 
