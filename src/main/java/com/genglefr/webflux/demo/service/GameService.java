abc7e368e78b1d0354dc5b003958528ccd6d8a40
genglefr <nufann@gmail.com>
2018-10-24 11:05:33 +0200

    SSL version

--- src/main/java/com/genglefr/webflux/demo/service/GameService.java
+++
@@ -1,8 +0,0 @@
-package com.genglefr.webflux.demo.service;
-
-import com.genglefr.webflux.demo.model.Game;
-import org.springframework.stereotype.Service;
-
-@Service
-public class GameService extends GenericService<Game> {
-}
